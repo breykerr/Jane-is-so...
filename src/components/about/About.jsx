251bee94fffd3e08fb010c185139ad88a612af77
breykerr <106961694+breykerr@users.noreply.github.com>
2023-02-07 06:54:22 +0600

    ...

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
         <span className="section__subtitle">Anything</span>
 
         <div className="about__container container grid">
-          <img src={JaneAboutMe} alt="photoJane" id="about__img"></img>
+          <div id="about__img"></div>
           <div className="about__data">
             <Info />
             <p className="about__desc">
